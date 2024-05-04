6c7c43500c5dc7bd8fb8e744aab8c5fa6e245018
none <none@DESKTOP-FKH17L5>
2024-05-04 16:12:40 -0300

    fin del ejercicio02

--- Tp2_AvendanoLeonel/src/ar/edu/unju/fi/ejercicio02/constantes/Mes.java
+++ Tp2_AvendanoLeonel/src/ar/edu/unju/fi/ejercicio02/constantes/Mes.java
@@ -1,5 +1,6 @@
 package ar.edu.unju.fi.ejercicio02.constantes;
 
-public class Mes {
-
+public enum Mes {
+	ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, AGOSTO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE
+	
 }
